581d91e26c513dc06ebf226bd8bfdaacb854ee08
sooham <rafizsooham@gmail.com>
2016-03-25 21:46:41 -0400

    added counter test file

--- src/counter.v
+++ src/counter.v
@@ -1,4 +1,4 @@
-`timescale 1 ps / 1 ps
+`timescale 1 ns / 1 ns
 
 // counter counting from 0 to 15 starting when start_count is 1
 
@@ -9,8 +9,8 @@ module counter(
     output reg [3:0] result // resulting output
     );
 
-    initial counting = 1'b1;
-    initial result = 4'd15;
+    initial counting = 1'b0;
+    initial result = 4'd0;
 
     always@(posedge clk) begin
         if (start_count) begin
@@ -23,11 +23,16 @@ module counter(
             if (result == 4'd15) begin
                 counting <= 1'b0;
                 result <= 4'd0;
-            end else begin
+            end else if (start_count) begin
+                // result = 0
+            end else
                 result <= result + 4'd1;
             end
         end
     end
 endmodule
+// TODO: change render_rect to give writeEn out from counter, not FSM,
+// otherwise infinite looping occurs
+// TODO: also change the ns back to ps after testing
 
 
---
+++ tests/counter.do
@@ -0,0 +1,31 @@
+cd ../src
+
+vlib work
+vlog counter.v
+vsim counter
+
+log {/*}
+add wave {/*}
+
+# testing counter
+
+force {clk} 0 0, 1 10 -r 20     # get clock running
+
+# test condition - start_count is 1 for one clock cycle and 0 for rest
+force {start_count} 0 1, 0 30
+run 340 ns
+
+force {start_count} 0
+run 60 ns
+
+# test condition - start_count is 1 for many intervals over cycle
+force {start_count} 400 1, 0 20 -r
+run 340 ns
+
+force {start_count} 0
+run 60 ns
+
+# start_count is always 1 throughout
+force {start_count} 1
+run 340 ns
+
