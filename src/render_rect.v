cf6ade849a72880dd1f613044172fc29b2024236
sooham <rafizsooham@gmail.com>
2016-03-27 14:55:04 -0400

    infinite looping bug fixed

--- src/render_rect.v
+++ src/render_rect.v
@@ -59,8 +59,22 @@ module datapath(
     );
 
     wire [16:0] offset;
+
     reg start_signal = 0;
-    counter c0(clk, enable, width * height, writeEn, offset);
+
+    // when enable turns on, start signal is set to 1
+    always @(posedge enable) begin
+            start_signal <= 1;
+    end
+
+    // on the next clk edge it is put back to 0
+    always@(posedge clk) begin
+        if (start_signal) start_signal <= 0;
+    end
+
+    // counter should not be fed in enable
+    counter c0(clk, start_signal, width * height, writeEn, offset);
+
     // load the output registers
     always@(*) begin
         if (!enable) begin
@@ -69,13 +83,8 @@ module datapath(
             color_stream = border ? border_color : back_color;
             writeEn = 0;
             done = 0;
-            start_signal = 0;
         end else begin
             // enable is on, start drawing the square
-            start_signal = 1;
-            if (start_signal == 1) begin
-                start_signal = 0;
-            end
             if (border) begin
                 // border offsets only occur when
                 x_stream = origin_x + ((((offset % width) == 9'b0) || ((offset % width) == width - 1) || ((offset / width) == 8'b0) || ((offset / width) == height - 1)) ? offset % width : 9'b0);
