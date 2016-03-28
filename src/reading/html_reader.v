f1f979561d7682471d29230f1e869310a0ae5445
Juan Camilo Osorio <jcoc611@gmail.com>
2016-03-28 15:46:03 -0400

    Fixed issues with html reader, tests work

--- src/reading/html_reader.v
+++ src/reading/html_reader.v
@@ -5,42 +5,39 @@
  * For other modules to use.
  */
 module html_reader(
-  input state_enable,                       // enable / ~reset
-  input clock,                              // clock
-  input pause,
+	input state_enable,                       // enable / ~reset
+	input clock,                              // clock
+	input pause,
 
-  output reg has_finished,                  // has it finished?
-  output reg [`CHAR_BITES] char             // output char stream
+	output reg has_finished,                  // has it finished?
+	output reg [`CHAR_BITES] char             // output char stream
 );
 
-  integer file, c;           // file to read, character
+	integer file, c;           // file to read, character
 
-  initial begin
-    file = $fopen("./html/index.html", "r");
-  end
+	initial char = 0;
+	initial has_finished = 0;
+	initial begin
+		file = $fopen("html/index.html", "r");
+	end
 
-  always@(clock) begin
-    if (state_enable) begin
-      if (has_finished == 0 && pause == 0) begin
-        if ($feof(file)) begin
-          has_finished <= 1;
-        end else begin
-          // read chars and check for eof
-          c <= $fgetc(file);
-        end
-      end
-    end else begin
-      // reset
-      char <= 0;
-      has_finished = 0;
-    end
-  end
+	always@(posedge clock) begin
+		if (state_enable) begin
+			if (has_finished == 0 && pause == 0) begin
+				// read chars and check for eof
+				c <= $fgetc(file);
+			end
+		end else begin
+			// reset
+			char <= 0;
+			has_finished = 0;
+		end
+	end
 
-  always@(c) begin
-    char = c[`CHAR_BITES];
-  end
-
-  initial begin
-    $fclose(file);  
-  end
+	always@(*) begin
+		char = c[`CHAR_BITES];
+		if ($feof(file)) begin
+			has_finished = 1;
+		end
+	end
 endmodule
--- tests/reading/html_reader.do
+++ tests/reading/html_reader.do
@@ -12,4 +12,10 @@ force {clock} 0 0, 1 1 -r 2
 # test case 1, enable is always 1 not paused
 force {state_enable} 1
 force {pause} 0
-run 400 ps
+run 100 ps
+
+force {pause} 1
+run 50ps
+
+force {pause} 0
+run 300ps
